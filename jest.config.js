f0ad113a555d2651e52a4027ca2eae7fec225311
Diederik van den Burger <diederikvandenburger@tab.capital>
2020-02-22 15:50:06 +0100

    test: enable coverage

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  coverageDirectory: 'tests/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
