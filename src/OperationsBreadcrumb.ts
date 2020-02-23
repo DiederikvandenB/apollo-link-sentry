25bc2263fa1666af28ae58f1e181f32eae523110
Diederik van den Burger <diederikvandenburger@tab.capital>
2020-02-23 14:20:16 +0100

    refactor: convert methods to fields holding arrow functions

--- src/OperationsBreadcrumb.ts
+++ src/OperationsBreadcrumb.ts
@@ -24,56 +24,56 @@ export class OperationsBreadcrumb {
    * Sets the breadcrumb's log level
    * @param level
    */
-  public level(level: Sentry.Severity): OperationsBreadcrumb {
+  level = (level: Sentry.Severity): OperationsBreadcrumb => {
     this.breadcrumb.level = level;
     return this;
-  }
+  };
 
   /**
    * Sets the breadcrumb's category, which is prefixed with `graphQL`
    * @param category
    */
-  public category(category?: ApolloLinkSentry.Breadcrumb.Category): OperationsBreadcrumb {
+  category = (category?: ApolloLinkSentry.Breadcrumb.Category): OperationsBreadcrumb => {
     this.breadcrumb.category = `gql ${category || ''}`.trim();
     return this;
-  }
+  };
 
   /**
    * Set the breadcrumb's message, normally the graphQL operation's name
    * @param message
    */
-  public message(message?: string): OperationsBreadcrumb {
+  message = (message?: string): OperationsBreadcrumb => {
     this.breadcrumb.message = message;
     return this;
-  }
+  };
 
   /**
    * Set the breadcrumb's extra data
    * @param data
    */
-  public data(data: ApolloLinkSentry.Operation.Data): OperationsBreadcrumb {
+  data = (data: ApolloLinkSentry.Operation.Data): OperationsBreadcrumb => {
     this.breadcrumb.data = {
       ...this.breadcrumb.data,
       ...data,
     };
 
     return this;
-  }
+  };
 
   /**
    * Set the breadcrumb's type, normally `http`
    * @param type
    */
-  public type(type: string): OperationsBreadcrumb {
+  type = (type: string): OperationsBreadcrumb => {
     this.breadcrumb.type = type;
     return this;
-  }
+  };
 
   /**
    * Fill the breadcrumb with information from an OperationsObserver
    * @param operation
    */
-  public fillFromOperation(operation: OperationsObserver): OperationsBreadcrumb {
+  fillFromOperation = (operation: OperationsObserver): OperationsBreadcrumb => {
     const data: ApolloLinkSentry.Operation.Data = trimObject({
       query: operation.query,
       cache: stringifyObject(operation.cache),
@@ -84,12 +84,12 @@ export class OperationsBreadcrumb {
       .message(operation.name)
       .category(operation.type)
       .data(data);
-  }
+  };
 
   /**
    * Tell Sentry about our breadcrumb
    */
-  public attachToEvent(): OperationsBreadcrumb {
+  attachToEvent = (): OperationsBreadcrumb => {
     if (this.flushed) {
       console.warn('[apollo-link-sentry] OperationsBreadcrumb.attachToEvent() was called on an already flushed breadrumb');
       return this;
@@ -99,13 +99,11 @@ export class OperationsBreadcrumb {
     this.flushed = true;
 
     return this;
-  }
+  };
 
   /**
    * Stringify the breadcrumb, used for debugging purposes
    */
   /* istanbul ignore next */
-  public toString(): string {
-    return stringifyObject(this.breadcrumb);
-  }
+  toString = (): string => stringifyObject(this.breadcrumb);
 }
