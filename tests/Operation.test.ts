aa33b909bf0818a3cfc39b1e4c8dbbe3cfaecc10
Diederik van den Burger <diederikvandenburger@tab.capital>
2020-02-25 11:04:53 +0100

    refactor: updates Operation.test.ts structure

--- tests/Operation.test.ts
+++ tests/Operation.test.ts
@@ -2,38 +2,40 @@ import { Operation } from '../src/Operation';
 
 import OperationStub from './stubs/Operation';
 
-it('is possible to create a new operation', () => {
-  expect(() => new Operation(OperationStub)).not.toThrow();
-});
+describe('Operation', () => {
+  it('should be possible to create a new operation', () => {
+    expect(() => new Operation(OperationStub)).not.toThrow();
+  });
 
-it('is possible to get the name from the operation', () => {
-  const operation = new Operation(OperationStub);
-  expect(operation['getName']()).toBe(OperationStub.operationName);
-});
+  it('should be possible to get the name', () => {
+    const operation = new Operation(OperationStub);
+    expect(operation['getName']()).toBe(OperationStub.operationName);
+  });
 
-it('is possible to get the type from the operation', () => {
-  const operation = new Operation(OperationStub);
+  it('should be possible to get the type', () => {
+    const operation = new Operation(OperationStub);
 
-  // @ts-ignore
-  const type = OperationStub.query.definitions[0].operation;
+    // @ts-ignore
+    const type = OperationStub.query.definitions[0].operation;
 
-  expect(operation['getType']()).toBe(type);
-});
+    expect(operation['getType']()).toBe(type);
+  });
 
-it('is possible to get the cache from the operation', () => {
-  const operation = new Operation(OperationStub);
-  const { data: cache } = OperationStub.getContext().cache.data;
-  expect(operation['getApolloCache']()).toEqual(cache);
-});
+  it('should be possible to get the cache', () => {
+    const operation = new Operation(OperationStub);
+    const { data: cache } = OperationStub.getContext().cache.data;
+    expect(operation['getApolloCache']()).toEqual(cache);
+  });
 
-it('is possible to get the variables from the operation', () => {
-  const operation = new Operation(OperationStub);
-  const { variables } = OperationStub;
-  expect(operation['getVariables']()).toEqual(variables);
-});
+  it('should be possible to get the variables', () => {
+    const operation = new Operation(OperationStub);
+    const { variables } = OperationStub;
+    expect(operation['getVariables']()).toEqual(variables);
+  });
 
-it('is possible to get the query from the operation', () => {
-  const operation = new Operation(OperationStub);
-  const query = OperationStub.query.loc?.source.body;
-  expect(operation['getQuery']()).toEqual(query);
+  it('should be possible to get the query', () => {
+    const operation = new Operation(OperationStub);
+    const query = OperationStub.query.loc?.source.body;
+    expect(operation['getQuery']()).toEqual(query);
+  });
 });
